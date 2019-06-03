d926ad4dd962faea33933cf8a787a582f4df7147
Jordan-M-Anderson <47698298+Jordan-M-Anderson@users.noreply.github.com>
2019-06-02 22:11:00 -0500

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SimonSays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Orange = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.Blue = new System.Windows.Forms.Button();
+            this.Green = new System.Windows.Forms.Button();
+            this.Purple = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.Yellow = new System.Windows.Forms.Button();
+            this.Score = new System.Windows.Forms.Label();
+            this.HighScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Orange
+            // 
+            this.Orange.AutoSize = true;
+            this.Orange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Orange.Location = new System.Drawing.Point(12, 12);
+            this.Orange.Name = "Orange";
+            this.Orange.Size = new System.Drawing.Size(75, 73);
+            this.Orange.TabIndex = 0;
+            this.Orange.UseVisualStyleBackColor = false;
+            this.Orange.Click += new System.EventHandler(this.Orange_Click);
+            // 
+            // start
+            // 
+            this.start.AutoSize = true;
+            this.start.Location = new System.Drawing.Point(12, 249);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(156, 43);
+            this.start.TabIndex = 6;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Blue
+            // 
+            this.Blue.AutoSize = true;
+            this.Blue.BackColor = System.Drawing.Color.Blue;
+            this.Blue.Location = new System.Drawing.Point(12, 91);
+            this.Blue.Name = "Blue";
+            this.Blue.Size = new System.Drawing.Size(75, 73);
+            this.Blue.TabIndex = 7;
+            this.Blue.UseVisualStyleBackColor = false;
+            this.Blue.Click += new System.EventHandler(this.Blue_Click);
+            // 
+            // Green
+            // 
+            this.Green.AutoSize = true;
+            this.Green.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Green.Location = new System.Drawing.Point(12, 170);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(75, 73);
+            this.Green.TabIndex = 8;
+            this.Green.UseVisualStyleBackColor = false;
+            this.Green.Click += new System.EventHandler(this.Green_Click);
+            // 
+            // Purple
+            // 
+            this.Purple.AutoSize = true;
+            this.Purple.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Purple.Location = new System.Drawing.Point(93, 12);
+            this.Purple.Name = "Purple";
+            this.Purple.Size = new System.Drawing.Size(75, 73);
+            this.Purple.TabIndex = 9;
+            this.Purple.UseVisualStyleBackColor = false;
+            this.Purple.Click += new System.EventHandler(this.Purple_Click);
+            // 
+            // Red
+            // 
+            this.Red.AutoSize = true;
+            this.Red.BackColor = System.Drawing.Color.Red;
+            this.Red.Location = new System.Drawing.Point(93, 91);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(75, 73);
+            this.Red.TabIndex = 10;
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Yellow
+            // 
+            this.Yellow.AutoSize = true;
+            this.Yellow.BackColor = System.Drawing.Color.Yellow;
+            this.Yellow.Location = new System.Drawing.Point(93, 170);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(75, 73);
+            this.Yellow.TabIndex = 11;
+            this.Yellow.UseVisualStyleBackColor = false;
+            this.Yellow.Click += new System.EventHandler(this.Yellow_Click);
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(12, 295);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(47, 13);
+            this.Score.TabIndex = 12;
+            this.Score.Text = "Score: 0";
+            // 
+            // HighScore
+            // 
+            this.HighScore.AutoSize = true;
+            this.HighScore.Location = new System.Drawing.Point(96, 295);
+            this.HighScore.Name = "HighScore";
+            this.HighScore.Size = new System.Drawing.Size(72, 13);
+            this.HighScore.TabIndex = 13;
+            this.HighScore.Text = "High Score: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(175, 313);
+            this.Controls.Add(this.HighScore);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Red);
+            this.Controls.Add(this.Purple);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Blue);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.Orange);
+            this.Name = "Form1";
+            this.Text = "Simon Says";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Orange;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button Blue;
+        private System.Windows.Forms.Button Green;
+        private System.Windows.Forms.Button Purple;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Yellow;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label HighScore;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimonSays
+{
+    public partial class Form1 : Form
+    {
+
+        private delegate void SafeCallDelegate(Button button, Color color);
+        Random colorGen = new Random();
+        List<int> colorList = new List<int>();
+
+        bool isActive = false;
+        int position = 0;
+        int highScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Generates the first color and then creates a new thread of showColors.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void start_Click(object sender, EventArgs e)
+        {
+            Score.Text = ("Score: 0");
+            isActive = true;
+
+            colorList = new List<int>();
+            colorList.Add(colorGen.Next(6));
+
+            new Thread(showColors).Start();
+        }
+
+        /// <summary>
+        /// Makes the button that corresponds to the colors in the list light up in order
+        /// with a delay.
+        /// </summary>
+        void showColors()
+        {
+            isActive = false;
+
+            foreach (int color in colorList)
+            {
+                //Will make the button that corresponds to the color in the
+                //colorList light up.
+                switch (color)
+                {
+                    case 0:
+                        //WriteButtonSafe(Orange, Color.Orange);
+                        Orange.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Orange.BackColor = Color.Orange;
+                        break;
+
+                    case 1:
+                        //WriteButtonSafe(Blue, Color.Blue);
+                        Blue.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Blue.BackColor = Color.Blue;
+                        break;
+
+                    case 2:
+                        //WriteButtonSafe(Green, Color.Green);
+                        Green.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Green.BackColor = Color.Green;
+                        break;
+
+                    case 3:
+                        //WriteButtonSafe(Purple, Color.Purple);
+                        Purple.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Purple.BackColor = Color.Purple;
+                        break;
+
+                    case 4:
+                        //WriteButtonSafe(Red, Color.Red);
+                        Red.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Red.BackColor = Color.Red;
+                        break;
+
+                    case 5:
+                        //WriteButtonSafe(Yellow, Color.Yellow);
+                        Yellow.BackColor = Color.Transparent;
+                        Thread.Sleep(200);
+                        Yellow.BackColor = Color.Yellow;
+                        break;
+                }
+                Thread.Sleep(200);
+
+            }
+
+            isActive = true;
+        }
+
+        /// <summary>
+        /// This method is called everytime a button is pressed to check
+        /// if that is the correct button. It controls the score and when
+        /// the user has lost.
+        /// </summary>
+        /// <param name="testColor">
+        /// testColor is the color corrsponding to 
+        /// the button that the user pressed
+        /// </param>
+        void isCorrect(int testColor)
+        {
+            if (!isActive)
+            {
+                return;
+            }
+            else if (colorList[position] != testColor)
+            {
+                MessageBox.Show("Game Over... Final Score: " + (colorList.Count - 1));
+                position = 0;
+                isActive = false;
+                colorList = new List<int>();
+                return;
+            }
+
+            position++;
+
+            Score.Text = ("Score: " + colorList.Count);
+
+            if (colorList.Count > highScore)
+            {
+                highScore++;
+                HighScore.Text = ("High Score: " + highScore);
+            }
+
+            if (position >= colorList.Count)
+            {
+                colorList.Add(colorGen.Next(6));
+                position = 0;
+                new Thread(showColors).Start();
+            }
+            
+        }
+
+        private void WriteButtonSafe(Button button, Color color)
+        {
+            if (button.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteButtonSafe);
+                Invoke(d, new object[] { button, color });
+                Thread.Sleep(200);
+            }
+            else
+            {
+                Thread.Sleep(200);
+                button.BackColor = Color.Transparent;
+                Thread.Sleep(1000);
+                button.BackColor = color;
+                Thread.Sleep(200);
+            }
+        }
+
+        private void Orange_Click(object sender, EventArgs e)
+        {
+            isCorrect(0);
+        }
+
+        private void Blue_Click(object sender, EventArgs e)
+        {
+            isCorrect(1);
+        }
+
+        private void Green_Click(object sender, EventArgs e)
+        {
+            isCorrect(2);
+        }
+
+        private void Purple_Click(object sender, EventArgs e)
+        {
+            isCorrect(3);
+        }
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            isCorrect(4);
+        }
+
+        private void Yellow_Click(object sender, EventArgs e)
+        {
+            isCorrect(5);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
